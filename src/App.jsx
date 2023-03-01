41eb4bed9dea0738e9cb310b5d1ac5ace76a8df2
rishitha123456789 <rishita2707@gmail.com>
2023-03-01 13:45:43 +0530

    react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+const App=()=>{
+    return(
+        <div>
+            <h1>hello react</h1>
+            <p>2+2</p>
+            <p>{2+2}</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam ipsum sed quo libero, earum cupiditate nam adipisci ducimus asperna</p>
+        </div>
+    );
+}
+export default App
